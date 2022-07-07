e80908596d08e161d10c778c4dd664eb0b56ef71
eslam khder <eslamkhder11@gmail.com>
2022-07-08 01:37:29 +0200

    create project

---
+++ src/main/java/com/spring/springsecurity/controller/StudentController.java
@@ -0,0 +1,15 @@
+package com.spring.springsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+
+    @GetMapping("/hi")
+    public String hiStudent(){
+        return "HI STUDENT";
+    }
+}
