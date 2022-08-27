3ebd1f0613f64fd664bdea08cf1d52b380b1db9e
eslam khder <eslamkhder11@gmail.com>
2022-08-27 19:28:21 +0200

    JWT Token Filter

---
+++ back-end/src/main/java/com/spring/springsecurity/constans/SecurityConstant.java
@@ -0,0 +1,6 @@
+package com.spring.springsecurity.constans;
+
+public class SecurityConstant {
+    public static final String KEY = "islam";
+    public static final String HEADER = "Authorization";
+}
